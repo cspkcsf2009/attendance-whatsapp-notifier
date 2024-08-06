6c81a0fabe8d2c9a81ad5e2a0b4a390ec541945c
cspkcsf2009 <cspkcsf2009@gmail.com>
2024-08-06 16:55:19 +0530

    Font size incresed in mob view

--- src/components/AttendanceTable.jsx
+++ src/components/AttendanceTable.jsx
@@ -7,9 +7,9 @@ const AttendanceTable = ({ initialNames, attendance, handleStatusChange }) => {
         <tr
             className={`transition-colors duration-300 ease-in-out hover:bg-gray-100 ${index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}`}
         >
-            <td className="px-2 py-2 sm:px-4 sm:py-3 text-xs sm:text-base text-gray-900 font-medium text-center">{index + 1}</td>
-            <td className="px-2 py-2 sm:px-4 sm:py-3 text-xs sm:text-base text-gray-900 font-medium text-center">{name}</td>
-            <td className="px-2 py-2 sm:px-4 sm:py-3 text-xs sm:text-base text-gray-900">
+            <td className="px-2 py-2 sm:px-4 sm:py-3 text-sm sm:text-base text-gray-900 font-medium text-center">{index + 1}</td>
+            <td className="px-2 py-2 sm:px-4 sm:py-3 text-sm sm:text-base text-gray-900 font-medium text-center">{name}</td>
+            <td className="px-2 py-2 sm:px-4 sm:py-3 text-sm sm:text-base text-gray-900">
                 <div className="flex flex-col sm:flex-row justify-center items-center space-y-2 sm:space-y-0 sm:space-x-4">
                     <label className="flex items-center justify-center space-x-2 w-full sm:w-auto">
                         <input
@@ -61,9 +61,9 @@ const AttendanceTable = ({ initialNames, attendance, handleStatusChange }) => {
         <table className="w-full divide-y divide-gray-300 mb-6 bg-white shadow-sm rounded-lg">
             <thead className="bg-gray-200 text-gray-700">
                 <tr>
-                    <th className="px-2 py-1 sm:px-4 sm:py-2 text-center text-xs sm:text-lg font-medium">No.</th>
-                    <th className="px-2 py-1 sm:px-4 sm:py-2 text-center text-xs sm:text-lg font-medium">Name</th>
-                    <th className="px-2 py-1 sm:px-4 sm:py-2 text-center text-xs sm:text-lg font-medium">Attendance</th>
+                    <th className="px-2 py-1 sm:px-4 sm:py-2 text-center text-sm sm:text-lg font-medium">No.</th>
+                    <th className="px-2 py-1 sm:px-4 sm:py-2 text-center text-sm sm:text-lg font-medium">Name</th>
+                    <th className="px-2 py-1 sm:px-4 sm:py-2 text-center text-sm sm:text-lg font-medium">Attendance</th>
                 </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
